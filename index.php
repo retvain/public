821d99b76bc5d399d2bfdbc0b3526fa022810697
retvain <retvain@gmail.com>
2021-01-30 22:49:21 +0500

    2 step

--- index.php
+++ index.php
@@ -1,4 +1,25 @@
 <?php
-echo ('hi world! ');
-$a = array("name" => 2);
-var_dump($a);
+
+namespace Theory;
+
+class Application
+{ 
+   private $name = "nick";
+   private $environment;
+
+   public function setName($name) {
+       return $this->name;
+   }
+
+   public function getName() {
+       return $this->name;
+   }
+
+}
+ 
+$app = new Application("name", "development");
+print_r($app->getName());
+print_r($app->setName("another name"));
+ 
+
+?>

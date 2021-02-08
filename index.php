f499f8df99f4c00e6fff6b55ea65753e154b492e
retvain <retvain@gmail.com>
2021-02-08 22:34:08 +0500

    get testing

--- index.php
+++ index.php
@@ -3,7 +3,10 @@
 namespace Theory;
 
 $id = $_GET['id'];
+$id_next = $id+1;
+$id_prev = $id-1;
 echo "Мой id = $id </br>";
+
 
 
 class Application
@@ -32,3 +35,7 @@ class Application
 $app = new Application("name", "development");
 print_r($app->getName());
 print_r($app->setName("another name"));
+?>
+
+<a href="index.php?id=<?php echo $id_prev ?>">Назад</a> |
+<a href="index.php?id=<?php echo $id_next ?>">Вперед</a>

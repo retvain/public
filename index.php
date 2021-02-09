2465d831aa2251c5fff9eba918e90b99a5bec418
retvain <retvain@gmail.com>
2021-02-09 16:58:31 +0500

    my blog step 1

--- index.php
+++ index.php
@@ -12,3 +12,8 @@ date_default_timezone_set('Asia/Yekaterinburg');
 //language settings
 setlocale(LC_ALL, 'ru_RU.UTF-8');
 mb_internal_encoding('UTF-8');
+
+//BD connect
+PDO($hostname,)
+mysqli_connect($hostname, $username, $password) or die('no connection with database');
+mysqli_query()

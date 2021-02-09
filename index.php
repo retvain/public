eb3623988a186234f2d113ef6ad7463d5ec3a252
retvain <retvain@gmail.com>
2021-02-09 14:30:25 +0500

    post testing

--- index.php
+++ index.php
@@ -1,7 +1,5 @@
 <?php
 
-namespace Theory;
-
 //$id = isset($_GET['id']) ? (int)$_GET['id'] : 1;
 
 if (isset($_GET['id']))
@@ -16,36 +14,47 @@ if (!($id >= 1 and $id <= 3)) {
 
 $id_next = $id + 1;
 $id_prev = $id - 1;
-echo "Мой id = $id </br>" , var_dump($id) , "</br>";
+echo "Мой id = $id </br>", print_r($_GET), "</br>";
 
 echo "<img src='img/$id.png'>" . "</br>" .
     "<a href='index.php?id=$id_prev'>Назад</a>" . " " . "<a href='index.php?id=$id_next'>Вперед</a>" . "</br>" . "</br>";
 
-var_dump($_POST);
+
+//var_dump($_POST);
+//echo "</br>";
 print_r($_POST);
+echo "</br>" . "</br>";
+?>
 
+<form method="post">
+    Имя: <input type = "text" name="Имя"></br>
+    Телефон: <input type="text" name="Телефон"></br>
+    <input type="submit" value="Перезвоните мне">
+</form>
 
-
-
+<?php
+/*
+namespace Theory;
 class Application
 {
 //private $handlers = [];
-private $name;
-private $environment;
+    private $name;
+    private $environment;
 
-public function __construct($name, $environment)
-{
-$this->name = $name;
-$this->environment = $environment;
-}
+    public function __construct($name, $environment)
+    {
+        $this->name = $name;
+        $this->environment = $environment;
+    }
 
-public function setName($name)
-{
-return $this->name = $name;
-}
+    public function setName($name)
+    {
+        return $this->name = $name;
+    }
 
-public function getName()
-{
-return $this->name;
+    public function getName()
+    {
+        return $this->name;
+    }
 }
-}
+*/
